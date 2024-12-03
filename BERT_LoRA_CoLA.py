c56cab8cdae2afb17ebd258f465f934bbb452440
AnthonyWuZYW <anthonyzeyu@gmail.com>
2024-12-02 23:14:44 -0800

    update

--- BERT_LoRA_CoLA.py
+++ BERT_LoRA_CoLA.py
@@ -50,12 +50,10 @@ data_collator = DataCollatorWithPadding(tokenizer)
 
 # Set up training arguments
 training_args = TrainingArguments(
-    output_dir="./results",  # Correct directory path
     learning_rate=2e-5,
     per_device_train_batch_size=16,
     num_train_epochs=3,
     weight_decay=0.01,
-    logging_dir="./logs",
     report_to="none",
 )
 
