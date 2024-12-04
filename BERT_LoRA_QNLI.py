42fb41c682f459ea0213003a1519683a4935c0c7
AnthonyWuZYW <anthonyzeyu@gmail.com>
2024-12-03 19:03:27 -0800

    update

--- BERT_LoRA_QNLI.py
+++ BERT_LoRA_QNLI.py
@@ -46,7 +46,6 @@ encoded_dataset.set_format(type="torch", columns=["input_ids", "attention_mask",
 train_dataset = encoded_dataset["train"]
 # reduce training set size to save time
 train_dataset = train_dataset.shuffle(seed=42).select(range(int(len(train_dataset) / 10)))
-
 test_dataset = encoded_dataset["validation"]
 
 # Data collator for dynamic padding
